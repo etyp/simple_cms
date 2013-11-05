94891595b95c65ae88b781ea064881c2f53b107b
typ90 <ettypaldos@gmail.com>
2013-11-05 16:50:38 -0500

    Added a new migration, updated models, created controllers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131105155515) do
+ActiveRecord::Schema.define(:version => 20131105211600) do
 
   create_table "admin_users", :force => true do |t|
     t.string   "first_name", :limit => 25
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(:version => 20131105155515) do
     t.datetime "updated_at",                                :null => false
     t.string   "username",   :limit => 25
   end
+
+  create_table "admin_users_pages", :id => false, :force => true do |t|
+    t.integer "admin_user_id"
+    t.integer "page_id"
+  end
+
+  add_index "admin_users_pages", ["admin_user_id", "page_id"], :name => "index_admin_users_pages_on_admin_user_id_and_page_id"
 
   create_table "pages", :force => true do |t|
     t.integer  "subject_id"
@@ -35,6 +42,16 @@ ActiveRecord::Schema.define(:version => 20131105155515) do
 
   add_index "pages", ["permalink"], :name => "index_pages_on_permalink"
   add_index "pages", ["subject_id"], :name => "index_pages_on_subject_id"
+
+  create_table "section_edits", :force => true do |t|
+    t.integer  "admin_user_id"
+    t.integer  "section_id"
+    t.string   "summary"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+  end
+
+  add_index "section_edits", ["admin_user_id", "section_id"], :name => "index_section_edits_on_admin_user_id_and_section_id"
 
   create_table "sections", :force => true do |t|
     t.integer  "page_id"
